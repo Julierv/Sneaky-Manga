0fa6a24e8c54e9fc3934c527329c6c76dfb69277
Jose Julian Sanchez Alarcon <46978616+Julierv@users.noreply.github.com>
2023-11-29 00:56:36 -0500

    gh-pages deploy

--- manga-app/src/index.js
+++ manga-app/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   root
 );
 
